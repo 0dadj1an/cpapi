4c38b9be75f8b9592504915cb9a8b17d061af3e9
Joshua Hatter <j.d.hatter10@gmail.com>
2017-05-25 20:02:51 -0500

    Brought back in api stuff

---
+++ cpapipackage/group.py
@@ -0,0 +1,70 @@
+#Import Post
+from cpapipackage.post import api_call
+import threading, time
+
+#Method for adding a group object
+def addgroup(usrdef_sship, groupname, groupcolor, sid):
+    new_group_data = {'name':groupname, 'color':groupcolor}
+    api_call(usrdef_sship, 443,'add-group', new_group_data ,sid)
+
+#Method to add group to group
+def addgroupgroup(usrdef_sship, addgroupname, groupname, sid):
+    addgroup_data = {'name':addgroupname, 'groups':groupname}
+    api_call(usrdef_sship, 443, 'set-group', addgroup_data, sid)
+
+#Method for retrieving all groups
+def getallgroups(usrdef_sship, sid):
+    count = 500
+    show_groups_data = {'offset':0, 'limit':500, 'details-level':'standard', 'order':[{'ASC':'name'}]}
+    show_groups_result = api_call(usrdef_sship, 443, 'show-groups', show_groups_data, sid)
+    allgrouplist = []
+    for groups in show_groups_result["objects"]:
+        allgrouplist.append(groups["name"])
+    while show_groups_result["to"] != show_groups_result["total"]:
+        show_groups_data = {'offset':count, 'limit':500, 'details-level':'standard'}
+        show_groups_result = api_call(usrdef_sship, 443, 'show-groups', show_groups_data, sid)
+        for groups in show_groups_result["objects"]:
+            allgrouplist.append(groups["name"])
+        count = count + 500
+    return (allgrouplist)
+
+#Method for adding a group object with members
+def addgroupmembers(usrdef_sship, groupname, members, sid):
+    new_group_data = {'name':groupname, 'members':members}
+    t1 = threading.Thread(target=api_call, args=(usrdef_sship, 443, 'add-group', new_group_data ,sid))
+    t1.start()
+    time.sleep(0.1)
+
+#Method to import group from csv
+def importgroups(usrdef_sship, filename, sid):
+    csvgroups = open(filename, "r").read().split()
+    for line in csvgroups:
+        if not line:
+            continue
+        groupname = line.split(',')
+        memberlist = groupname[1].split(';')
+        addgroupmembers(usrdef_sship, groupname[0], memberlist[0:-1], sid)
+
+#Method to export host to csv file
+def exportgroups(usrdef_sship, sid):
+    count = 500
+    show_groups_data = {'offset':0, 'limit':500, 'details-level':'full', 'order':[{'ASC':'name'}]}
+    show_groups_result = api_call(usrdef_sship, 443, 'show-groups', show_groups_data ,sid)
+    groupsexportfile = open(("exportedgroups.csv"), "w+")
+    for group in show_groups_result["objects"]:
+        groupsexportfile.write(group["name"] + ",")
+        listofmembers = group["members"]
+        for member in listofmembers:
+            groupsexportfile.write(member["name"] + ";")
+        groupsexportfile.write("\n")
+    while show_groups_result["to"] != show_groups_result["total"]:
+        show_groups_data = {'offset':count, 'limit':500, 'details-level':'full', 'order':[{'ASC':'name'}]}
+        show_groups_result = api_call(usrdef_sship, 443, 'show-groups', show_groups_data ,sid)
+        for group in show_groups_result["objects"]:
+            groupsexportfile.write(group["name"] + ",")
+            listofmembers = group["members"]
+            for member in listofmembers:
+                groupsexportfile.write(member["name"] + ";")
+            groupsexportfile.write("\n")
+        count = count + 500
+    groupsexportfile.close()
---
+++ cpapipackage/service.py
@@ -0,0 +1,83 @@
+#Import Post
+from cpapipackage.post import api_call
+
+#Method to export tcp services to csv file
+def exporttcpservices(usrdef_sship, sid):
+    show_tcp_data = {'limit':500, 'details-level':'full', 'order':[{'ASC':'name'}]}
+    show_tcp_result = api_call(usrdef_sship, 443, 'show-services-tcp', show_tcp_data ,sid)
+    tcpexport = open(("exportedtcpsrv.csv"), "w+")
+    for service in show_tcp_result["objects"]:
+        if service["domain"]["name"] == 'SMC User':
+            if 'source-port' in service:
+                sp = service["source-port"]
+            else:
+                sp = 'none'
+            tcpexport.write(service["name"] + ";" + str(service["port"]) + ";" + str(service["keep-connections-open-after-policy-installation"]) + ";" +
+                            str(service["session-timeout"]) + ";" + str(sp) + ";" + str(service["match-for-any"]) + ";" +
+                            str(service["sync-connections-on-cluster"]) + ";" + service["color"] + ";" + str(service["aggressive-aging"]) + "\n")
+        elif service["domain"]["name"] == 'Check Point Data':
+            break
+        else:
+            print ("uh oh")
+    tcpexport.close()
+
+#Method for adding tcp service for importtcpservice
+def importaddtcp(usrdef_sship, name, port, kcoapi, st, sp, mfa, sync, srvcol, aa, sid):
+    aa = eval(aa)
+    if sp == 'none':
+        new_tcp_data = {'name':name, 'port':port, 'keep-connections-open-after-policy-installation':kcoapi, 'session-timeout':st, 'match-for-any':mfa,
+                        'sync-connections-on-cluster':sync, 'color':srvcol, 'aggressive-aging':aa}
+    else:
+        new_tcp_data = {'name':name, 'port':port, 'keep-connections-open-after-policy-installation':kcoapi, 'session-timeout':st, 'source-port':sp,
+                        'match-for-any':mfa, 'sync-connections-on-cluster':sync, 'color':srvcol, 'aggressive-aging':aa}
+    api_call(usrdef_sship, 443, 'add-service-tcp', new_tcp_data, sid)
+
+#Method to import tcp service from csv file
+def importtcpservice(usrdef_sship, filename, sid):
+    csvtcp = open(filename, "r").read().split("\n")
+    for line in csvtcp:
+        if not line:
+            continue
+        apiprep = line.split(';')
+        importaddtcp(usrdef_sship, apiprep[0], apiprep[1], apiprep[2], apiprep[3], apiprep[4], apiprep[5], apiprep[6], apiprep[7], apiprep[8], sid)
+
+#Method to export udp services to csv file
+def exportudpservices(usrdef_sship, sid):
+    show_udp_data = {'limit':500, 'details-level':'full', 'order':[{'ASC':'name'}]}
+    show_udp_result = api_call(usrdef_sship, 443, 'show-services-udp', show_udp_data ,sid)
+    udpexport = open(("exportedudpsrv.csv"), "w+")
+    for service in show_udp_result["objects"]:
+        if service["domain"]["name"] == 'SMC User':
+            if 'source-port' in service:
+                sp = service["source-port"]
+            else:
+                sp = 'none'
+            udpexport.write(service["name"] + ";" + str(service["port"]) + ";" + str(service["accept-replies"]) + ";" +
+                            str(service["keep-connections-open-after-policy-installation"]) + ";" +
+                            str(service["session-timeout"]) + ";" + str(sp) + ";" + str(service["match-for-any"]) + ";" +
+                            str(service["sync-connections-on-cluster"]) + ";" + service["color"] + ";" + str(service["aggressive-aging"]) + "\n")
+        elif service["domain"]["name"] == 'Check Point Data':
+            break
+        else:
+            print ("uh oh")
+    udpexport.close()
+
+#Method for adding udp service for importudpservice
+def importaddudp(usrdef_sship, name, port, ar, kcoapi, st, sp, mfa, sync, srvcol, aa, sid):
+    aa = eval(aa)
+    if sp == 'none':
+        new_udp_data = {'name':name, 'port':port, 'keep-connections-open-after-policy-installation':kcoapi, 'session-timeout':st, 'match-for-any':mfa,
+                        'sync-connections-on-cluster':sync, 'color':srvcol, 'aggressive-aging':aa}
+    else:
+        new_udp_data = {'name':name, 'port':port, 'accept-replies':ar, 'keep-connections-open-after-policy-installation':kcoapi, 'session-timeout':st, 'source-port':sp,
+                        'match-for-any':mfa, 'sync-connections-on-cluster':sync, 'color':srvcol, 'aggressive-aging':aa}
+    api_call(usrdef_sship, 443, 'add-service-udp', new_udp_data, sid)
+
+#Method to import udp service from csv file
+def importudpservice(usrdef_sship, filename, sid):
+    csvudp = open(filename, "r").read().split("\n")
+    for line in csvudp:
+        if not line:
+            continue
+        apiprep = line.split(';')
+        importaddudp(usrdef_sship, apiprep[0], apiprep[1], apiprep[2], apiprep[3], apiprep[4], apiprep[5], apiprep[6], apiprep[7], apiprep[8], apiprep[9], sid)
