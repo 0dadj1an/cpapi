77c305f58bc9528a02d56ad681882814ffc94fbc
Joshua Hatter <j.d.hatter10@gmail.com>
2017-05-28 21:36:59 -0500

    Policy Log output and progress on r8010 new json track field settings.

--- cpapipackage/policy.py
+++ cpapipackage/policy.py
@@ -57,6 +57,9 @@ def getalllayers(usrdef_sship, package, sid):
 def exportrules(usrdef_sship, package, layer, sid):
     show_rulebase_data = {'package':package, 'name':layer, 'details-level':'standard', 'use-object-dictionary':'true'}
     show_rulebase_result = api_call(usrdef_sship, 443, 'show-access-rulebase', show_rulebase_data ,sid)
+    policylog = open((".\logs\policy.log"), "w+")
+    policylog.write(str(show_rulebase_result))
+    policylog.close()
     rulebaseexport = open(("exportedrules.csv"), "w+")
     for rule in show_rulebase_result["rulebase"]:
         countersrc = 0
@@ -72,7 +75,7 @@ def exportrules(usrdef_sship, package, layer, sid):
         dst = rule["destination"]
         srv = rule["service"]
         act = rule["action"]
-        trc = rule["track"]
+        trc = rule["track"]["type"]
         trg = rule["install-on"]
         for obj in show_rulebase_result["objects-dictionary"]:
             if name == obj["uid"]:
