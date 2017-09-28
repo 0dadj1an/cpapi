63d74e9521b01ddeb4e612803b8169e82d275d25
themadhatterz <j.d.hatter10@gmail.com>
2017-09-28 12:58:32 -0500

    bus

--- cpapiweb/app/views.py
+++ cpapiweb/app/views.py
@@ -5,5 +5,5 @@ from flask import request
 @app.route('/login', methods=['POST', 'GET'])
 def login():
     if request.method == 'POST':
-        print (request.data)
+        print (request.get_data())
     return (render_template('login.html', title='Sign In'))
