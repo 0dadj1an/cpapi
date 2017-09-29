f6174d5ee40608ce6e3eb70995310d2af8a3c6b0
Joshua Hatter <j.d.hatter10@gmail.com>
2017-09-29 07:19:52 -0500

    Update post.py

--- cpapiweb/cap/post.py
+++ cpapiweb/cap/post.py
@@ -1,7 +1,5 @@
 import json, requests
 
-requests.packages.urllib3.disable_warnings()
-
 def api_call(ipaddress, port, command, json_payload, sid):
     url = 'https://' + str(ipaddress) + ':' + str(port) + '/web_api/' + command
     if sid == None:
@@ -9,6 +7,7 @@ def api_call(ipaddress, port, command, json_payload, sid):
     else:
         request_headers = {'Content-Type' : 'application/json', 'X-chkp-sid' : sid}
     try:
+        requests.packages.urllib3.disable_warnings()
         response = requests.post(url, data=json.dumps(json_payload), headers=request_headers, timeout=(30, 300), verify=False)
         return(response.json())
     except requests.exceptions.RequestException as e:
