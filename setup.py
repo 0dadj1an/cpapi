62d38b71cf6bfd358105c21a1f7aeb2d3f8bd7ff
Joshua Hatter <j.d.hatter10@gmail.com>
2017-10-01 19:26:00 -0500

    more pkg

--- setup.py
+++ setup.py
@@ -14,17 +14,12 @@ setup(
     # Packages
     packages=['cpapiweb'],
 
-    # Include additional files into the package
-    include_package_data=True,
-
     # Details
     url='https://github.com/themadhatterz/cpapi',
 
     # License
     license='LICENSE.txt',
     description='Web interface for interacting with Check Point Software Management API.',
-
-    long_description='README.md',
 
     # Dependent packages (distributions)
     install_requires=[
