a2f906debd295339339f06b2b1337ca68fa574a1
Joshua Hatter <j.d.hatter10@gmail.com>
2017-10-02 11:24:24 -0500

    RCE patched

--- cpapiweb/cap/misc.py
+++ cpapiweb/cap/misc.py
@@ -1,7 +1,15 @@
 from cap.post import api_call
+import ast
 
 def customcommand(ipaddress, command, payload, sid):
-    payload = eval(payload)
+    try:
+        payload = ast.literal_eval(payload)
+    except ValueError:
+        response = 'Invalid input provided.'
+        return(response)
+    except Exception as exc:
+        response = exc
+        return(exc)
     custcomm_data = payload
     response = api_call(ipaddress, 443, command, custcomm_data, sid)
     return(response)
