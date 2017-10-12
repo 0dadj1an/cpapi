a7924ba56c835ccb7e5cc3f69eaec416a19f3ab5
themadhatterz <j.d.hatter10@gmail.com>
2017-10-12 17:26:52 -0500

    test me

--- test.sh
+++ test.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 python3 cpapiweb/run.py &
+
+sleep 5
+
 CODE=$(curl -s -I -X GET http://127.0.0.1:8080/login | awk '/HTTP/{print $2}')
+
+echo $(CODE)
+
 if [[ $CODE == "200" ]]; then
     exit
 else
