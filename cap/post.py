7d4765daaaeac1c43186eb44dec94c584d0e281b
Joshua Hatter <j.d.hatter10@gmail.com>
2018-03-08 08:21:52 -0600

    Version tagging updates.

--- cap/post.py
+++ cap/post.py
@@ -11,12 +11,12 @@ def api_call(ipaddress, port, command, json_payload, sid):
     if sid == None:
         request_headers = {
             'Content-Type': 'application/json',
-            'User-Agent': 'CPAPI v1.3.0'
+            'User-Agent': 'CPAPI v1.3.1'
         }
     else:
         request_headers = {
             'Content-Type': 'application/json',
-            'User-Agent': 'CPAPI v1.3.0',
+            'User-Agent': 'CPAPI v1.3.1',
             'X-chkp-sid': sid
         }
     try:
